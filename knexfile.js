3d63872071ba1878706f3d0d2ae0094c609d8ee1
Steve Rumizen <steverumizen@gmail.com>
2019-08-19 16:40:22 -0600

    Setup knex and create initial migration
    Co-Authored-By: Andreea Hanson <andreea.hanson7@gmail.com>

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+// Update with your config settings.
+module.exports = {
+  development: {
+    client: "pg",
+    connection: "postgres://localhost/swatchr",
+    useNullAsDefault: true
+  },
+  // production: {
+  //   client: "pg",
+  //   connection: "postgres://localhost/swatchr",
+  //   useNullAsDefault: true
+  // }
+};
