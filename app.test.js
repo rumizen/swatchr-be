4936001b72377da60e1ce098b5684fa2ba24a11d
Steve Rumizen <steverumizen@gmail.com>
2019-08-20 14:21:25 -0600

    Add all tests for happy path on get all projects
    Co-Authored-By: Andreea Hanson <andreea.hanson7@gmail.com.

--- app.test.js
+++ app.test.js
@@ -13,8 +13,20 @@ describe("API", () => {
         const res = await request(app).get("/api/v1/projects");
         expect(res.status).toBe(200);
       })
-      it("should return all projects in database", async () => {
-        
+      it.skip("should return all projects in database", async () => {
+        const expectedProjects = await database("projects").select();
+
+        const res = await request(app).get("/api/v1/projects");
+        const projects = res.body;
+
+        expect(projects).toEqual(expectedProjects);
+      })
+      it("should return a project with the query name", async () => {
+        const expectedProjectName = "Sample 1"
+        const res = await request(app).get("/api/v1/projects/?name=Sample 1");
+        const projectName = res.body[0].name;
+
+        expect(projectName).toEqual(expectedProjectName);
       })
     })
 
